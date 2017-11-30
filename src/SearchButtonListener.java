93a62736bece37f4a84b28485a435839133720c2
larryelantz <umbranox@ufl.edu>
2017-11-30 02:19:56 -0500

    Update SearchButtonListener.java

--- src/SearchButtonListener.java
+++ src/SearchButtonListener.java
@@ -1,11 +1,24 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+
 import javax.swing.JOptionPane;
+
+import twitter4j.TwitterException;
 
 public class SearchButtonListener implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JOptionPane.showMessageDialog(null, "Polling");
-		
+		try {
+			Interface.output();
+		} catch (TwitterException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 }
+
